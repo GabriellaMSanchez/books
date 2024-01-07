055755daeea26f57aa992f29a4cb39a06782960c
Gabriella M Sanchez <83431411+GabriellaMSanchez@users.noreply.github.com>
2024-01-07 14:49:42 -0800

    Add internal API to update book objects (#2)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 function App() {
+    // Holds all books objects in state.
     const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
+    // Fetch existing book list.
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    };
+
+    // Make sure we only call fetchBooks once when the app is first rendered.
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    // Sends request to create a book.
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
         const updatedBooks = [
             ...books,
-            { 
-                id: Math.round(Math.random()*9999), 
-                title 
-            }];
+            response.data
+        ];
         setBooks(updatedBooks);
     };
-    const deleteBookById = (id) => {
+
+    // Sends request to delete a book by id.
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
         setBooks(updatedBooks);
     };
 
-    const editBookById = (id, newTitle) => {
+    // Sends request to edit a book by the id.
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title:newTitle};
+                return { ...book, ...response.data};
             }
             return book;
         });
